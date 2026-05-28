5d411e730e384a1c6e53c0e3f31fb2a9edcaf975
Laura Mancin <lauramancin8@gmail.com>
2026-05-28 14:33:22 -0300

    fix/adicionando o popup e começando a estilização

--- gatos/botaoAdotar.js
+++ gatos/botaoAdotar.js
@@ -6,6 +6,8 @@ function fecharModal(){
     document.getElementById('modal').classList.remove('ativo');
 }
 
+
+//fecha se o usuario clicar fora do popup
 document.getElementById('modal').addEventListener('click', function(e) {
     if (e.target === this) fecharModal();
 })
