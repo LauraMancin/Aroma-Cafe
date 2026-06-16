00a3739c4420576800b499553d92797101718fcf
Mariane Carvalho Dearo <marianecarvalhodearo@gmail.com>
2026-06-16 16:07:26 -0300

    finalizando o formulário e colocando o Pop Up

--- script.js
+++ script.js
@@ -42,3 +42,17 @@ document.getElementById("estado").addEventListener("change", function() {
     });
 
 });
+
+function abrirModal() {
+    document.getElementById('modal').classList.add('ativo');
+}
+
+function fecharModal(){
+    document.getElementById('modal').classList.remove('ativo');
+}
+
+
+//fecha se o usuario clicar fora do popup
+document.getElementById('modal').addEventListener('click', function(e) {
+    if (e.target === this) fecharModal();
+})
